56c05e1edb1eb5d9d3e7df5f3c403f6a7a50992e
Japhet Ong <japhetlee.dev@gmail.com>
2024-04-28 20:47:03 +0800

    Add project files.

---
+++ Database/BlogSiteDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogsCodeFirst.Database
+{
+    public class BlogSiteDbContext : DbContext
+    {
+        public BlogSiteDbContext()
+        {
+
+        }
+
+        public BlogSiteDbContext(DbContextOptions<BlogSiteDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
